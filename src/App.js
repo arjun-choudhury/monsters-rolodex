734e8a43234b72704691bc2cb7ccc5647897d10a
Arjun Choudhury <arjun.choudhury.my@gmail.com>
2019-09-11 23:58:03 +1000

    Made 1 change to App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Jai Shri RAM
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
