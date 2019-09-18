9c5f350caa67c1d1d16e3f43f8e960d75e513baa
Arjun Choudhury <arjun.choudhury.my@gmail.com>
2019-09-18 22:36:34 +1000

    Modified App.js to remove initial create-react-app code & delete logo.svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,28 +6,19 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Jai Mata Di'
+      string: 'Jai Shri RAM'
     };
   }
 
   render() {
     return (<div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           { this.state.string }
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-      </a>
+        </p>       
       </header>
     </div>)
   }
