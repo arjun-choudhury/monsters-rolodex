feeed7189bc50e67e0c1bc207a4edff6235fc435
Arjun Choudhury <arjun.choudhury.my@gmail.com>
2019-09-22 19:23:16 +1000

    S-3 & C-34 - V1 - Search functionality added. Also refer to this code to learn how to apply filter() method on an array

--- src/App.js
+++ src/App.js
@@ -21,18 +21,17 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
         <input
           type='Search'
           placeholder='Search Monsters'
-          onChange={e => {
-            this.setState({ searchField: e.target.value },
-              () => {
-                console.log(this.state)
-              })
-          }} />
-        <CardList monsters={this.state.monsters} />
+          onChange={e => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
