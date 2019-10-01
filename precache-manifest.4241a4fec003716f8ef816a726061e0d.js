05bd3afe4925b544ef4c3ccb2c5b06855a770d71
Arjun Choudhury <arjun.choudhury.my@gmail.com>
2019-10-01 23:18:54 +1000

    Updates

---
+++ precache-manifest.4241a4fec003716f8ef816a726061e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9a7c171c6df96f33069e94585b5f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e83beb1711fe4f7dcd8b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c4c0bfe41a7d6e594382",
+    "url": "/static/js/2.179b6797.chunk.js"
+  },
+  {
+    "revision": "e83beb1711fe4f7dcd8b",
+    "url": "/static/js/main.6ff40f78.chunk.js"
+  },
+  {
+    "revision": "8382c191e6337420d07b",
+    "url": "/static/js/runtime~main.85dca0f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
